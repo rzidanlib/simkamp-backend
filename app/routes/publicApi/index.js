4e144b408e3f7ad329fa5c5a3ad72d5da42282d8
zdn <library.devz@gmail.com>
2024-07-03 08:05:19 +0700

    add: vercel.json

--- app/routes/publicApi/index.js
+++ app/routes/publicApi/index.js
@@ -4,5 +4,10 @@ import authController from "../../controllers/auth-controller.js";
 const publicAPI = new express.Router();
 
 publicAPI.post("/auth/login", authController.login);
+publicAPI.post("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Public API",
+  });
+});
 
 export { publicAPI };
